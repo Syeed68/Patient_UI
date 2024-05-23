59dcfb5a148e53839a862ce587765e5e143cb5c9
Syeed68 <abu.syeed968@gmail.com>
2024-05-24 00:03:13 +0600

    2nd commit

--- obj/Debug/net8.0/UI_PatientManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/UI_PatientManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI_PatientManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5068deae05ef15cdbf0779a18fd542a23c39da0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI_PatientManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI_PatientManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
