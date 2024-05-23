b5068deae05ef15cdbf0779a18fd542a23c39da0
Syeed68 <abu.syeed968@gmail.com>
2024-05-23 21:54:03 +0600

    first commit

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI_PatientManagementSystem.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
